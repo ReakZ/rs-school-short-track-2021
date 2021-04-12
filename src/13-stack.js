c3117d3c536dc66aa6a97253d988e316694c3e96
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-04-12 21:21:44 +0300

    feat: implement #13 task solution

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,20 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  push(element) {
+    if (!this.arr) {
+      this.arr = [element];
+    } else {
+      this.arr[this.arr.length] = element;
+    }
   }
 
   pop() {
-    throw new Error('Not implemented');
+    return this.arr.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.arr[this.arr.length - 1];
   }
 }
 
