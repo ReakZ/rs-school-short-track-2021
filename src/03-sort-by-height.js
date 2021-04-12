c8d4c7938c93229ccaad8df013ce08d804c7d1b8
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-04-12 20:51:22 +0300

    feat: implement #3 task solution

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,33 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const indices = [];
+  const array = [...arr];
+  const element = -1;
+  let idx = array.indexOf(element);
+  function sorting(a, b) {
+    if (a === -1 || b === -1) {
+      return 0;
+    }
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    return 0;
+  }
+  while (idx !== -1) {
+    indices.push(idx);
+    idx = array.indexOf(element, idx + 1);
+  }
+  const meh = array.filter((x) => x !== -1);
+  const rr = meh.sort(sorting);
+  indices.forEach((x) => {
+    rr.splice(x, 0, -1);
+  });
+  return rr;
 }
 
 module.exports = sortByHeight;
