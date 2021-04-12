72a5b5eb0878cf60a7ccb31a1c4718fac5fb588f
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-04-12 21:03:01 +0300

    feat: implement #7 task solution

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -11,10 +11,27 @@
  * @example
  * For input ["file", "file", "image", "file(1)", "file"],
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
+ *['doc', 'doc', 'image', 'doc(1)', 'doc']), ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  if (names.length === 0) return [];
+
+  const result = [...names];
+  for (let index = 0; index < result.length; index++) {
+    for (let index2 = index; index2 > 0; index2--) {
+      if (result[index] === result[index2 - 1]) {
+        if (result[index2][result[index2].length - 1] === ')') {
+          const num = result[index2][result[index2].length - 2];
+          result[index] = `${result[index]}(${
+            parseInt(num, 10) + 1
+          })`;
+        } else {
+          result[index] = `${result[index]}(1)`;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = renameFiles;
