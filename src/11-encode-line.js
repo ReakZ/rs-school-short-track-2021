2f23ed0b1b72f5b60cf59dfaf39f7907d01b7869
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-04-12 21:20:05 +0300

    feat: implement #11 task solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str === '') return '';
+  const array = str.split('');
+  const s = [array[0]];
+  const result = [s];
+  let count = 0;
+  for (let index = 1; index < array.length; index++) {
+    if (array[index - 1] === array[index]) {
+      result[count].push(array[index]);
+    } else {
+      count += 1;
+      result.push([array[index]]);
+    }
+  }
+  let res = '';
+  result.forEach((x) => {
+    res += x.length > 1 ? `${x.length}${x[0]}` : `${x[0]}`;
+  });
+  return res;
 }
 
 module.exports = encodeLine;
